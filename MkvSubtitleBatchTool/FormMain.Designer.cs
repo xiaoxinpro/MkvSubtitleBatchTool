6c55e6661b19be710ebf2fe36d6d5a123e181688
Chishin <pro@xxgzs.org>
2019-09-17 19:42:54 +0800

    初始化布局界面

--- MkvSubtitleBatchTool/FormMain.Designer.cs
+++ MkvSubtitleBatchTool/FormMain.Designer.cs
@@ -28,21 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listViewTrack = new System.Windows.Forms.ListView();
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPageSimpleFile = new System.Windows.Forms.TabPage();
+            this.tabPageBatchFile = new System.Windows.Forms.TabPage();
+            this.btnMixedFlow = new System.Windows.Forms.Button();
+            this.tabControlMain.SuspendLayout();
+            this.tabPageSimpleFile.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(68, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(416, 21);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenFile.Location = new System.Drawing.Point(490, 10);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "打开文件";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(8, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "MKV文件：";
+            // 
+            // listViewTrack
+            // 
+            this.listViewTrack.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewTrack.HideSelection = false;
+            this.listViewTrack.Location = new System.Drawing.Point(6, 39);
+            this.listViewTrack.Name = "listViewTrack";
+            this.listViewTrack.Size = new System.Drawing.Size(559, 256);
+            this.listViewTrack.TabIndex = 4;
+            this.listViewTrack.UseCompatibleStateImageBehavior = false;
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControlMain.Controls.Add(this.tabPageSimpleFile);
+            this.tabControlMain.Controls.Add(this.tabPageBatchFile);
+            this.tabControlMain.Location = new System.Drawing.Point(3, 3);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.Padding = new System.Drawing.Point(8, 3);
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(579, 358);
+            this.tabControlMain.TabIndex = 5;
+            // 
+            // tabPageSimpleFile
+            // 
+            this.tabPageSimpleFile.Controls.Add(this.btnMixedFlow);
+            this.tabPageSimpleFile.Controls.Add(this.listViewTrack);
+            this.tabPageSimpleFile.Controls.Add(this.btnOpenFile);
+            this.tabPageSimpleFile.Controls.Add(this.txtPath);
+            this.tabPageSimpleFile.Controls.Add(this.label1);
+            this.tabPageSimpleFile.Location = new System.Drawing.Point(4, 22);
+            this.tabPageSimpleFile.Name = "tabPageSimpleFile";
+            this.tabPageSimpleFile.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageSimpleFile.Size = new System.Drawing.Size(571, 332);
+            this.tabPageSimpleFile.TabIndex = 0;
+            this.tabPageSimpleFile.Text = "单文件处理";
+            this.tabPageSimpleFile.UseVisualStyleBackColor = true;
+            // 
+            // tabPageBatchFile
+            // 
+            this.tabPageBatchFile.Location = new System.Drawing.Point(4, 22);
+            this.tabPageBatchFile.Name = "tabPageBatchFile";
+            this.tabPageBatchFile.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageBatchFile.Size = new System.Drawing.Size(571, 332);
+            this.tabPageBatchFile.TabIndex = 1;
+            this.tabPageBatchFile.Text = "批量处理";
+            this.tabPageBatchFile.UseVisualStyleBackColor = true;
+            // 
+            // btnMixedFlow
+            // 
+            this.btnMixedFlow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMixedFlow.Location = new System.Drawing.Point(490, 301);
+            this.btnMixedFlow.Name = "btnMixedFlow";
+            this.btnMixedFlow.Size = new System.Drawing.Size(75, 23);
+            this.btnMixedFlow.TabIndex = 5;
+            this.btnMixedFlow.Text = "混流";
+            this.btnMixedFlow.UseVisualStyleBackColor = true;
+            this.btnMixedFlow.Click += new System.EventHandler(this.btnMixedFlow_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.tabControlMain);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MKV字幕批量提取与嵌入工具";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.tabControlMain.ResumeLayout(false);
+            this.tabPageSimpleFile.ResumeLayout(false);
+            this.tabPageSimpleFile.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewTrack;
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabPageSimpleFile;
+        private System.Windows.Forms.TabPage tabPageBatchFile;
+        private System.Windows.Forms.Button btnMixedFlow;
     }
 }
 
--- MkvSubtitleBatchTool/FormMain.cs
+++ MkvSubtitleBatchTool/FormMain.cs
@@ -6,14 +6,80 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
 
 namespace MkvSubtitleBatchTool
 {
     public partial class FormMain : Form
     {
+        /// <summary>
+        /// 当前目录含"\"
+        /// </summary>
+        private string MainPath;
+
+        #region 初始化相关
+        /// <summary>
+        /// 初始化
+        /// </summary>
         public FormMain()
         {
             InitializeComponent();
+            MainPath = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
         }
+
+        /// <summary>
+        /// 加载窗体事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.Text += @" V" + Application.ProductVersion.ToString();
+
+            InitFileListView(listViewTrack);
+        }
+        #endregion
+
+        #region 列表相关函数
+        /// <summary>
+        /// 初始化列表
+        /// </summary>
+        /// <param name="listView"></param>
+        private void InitFileListView(ListView listView)
+        {
+            //基本属性设置
+            listView.Clear();
+            listView.FullRowSelect = true;
+            listView.GridLines = true;
+            listView.CheckBoxes = true;
+            listView.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            listView.View = View.Details;
+
+            //创建列表头
+            listView.Columns.Add("序号", 60, HorizontalAlignment.Center);
+            listView.Columns.Add("文件名称", 100, HorizontalAlignment.Left);
+            listView.Columns.Add("文件路径", 300, HorizontalAlignment.Left);
+
+            //自动列宽
+            listView.Columns[2].Width = -2;//根据标题设置宽度
+        }
+        #endregion
+
+        #region 窗体控件相关
+
+        /// <summary>
+        /// 混流按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnMixedFlow_Click(object sender, EventArgs e)
+        {
+            Mkvinfo mkvinfo = new Mkvinfo();
+            mkvinfo.Get(MainPath + @"video\max.mkv");
+        }
+
+        #endregion
+
     }
 }
