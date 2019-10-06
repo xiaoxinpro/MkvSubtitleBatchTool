1b02d98cbd0080f3a85bc13b0b52eb54691c79ce
Chishin <pro@xxgzs.org>
2019-10-06 23:22:12 +0800

    增加MKV混流类

---
+++ MkvSubtitleBatchTool/Mkvmerge.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MkvSubtitleBatchTool
+{
+    /// <summary>
+    /// MKV混流类
+    /// </summary>
+    public class Mkvmerge
+    {
+        #region 字段
+
+        #endregion
+
+        #region 属性
+
+        #endregion
+
+        #region 构造函数
+
+        #endregion
+
+        #region 公共函数
+
+        #endregion
+
+    }
+}
